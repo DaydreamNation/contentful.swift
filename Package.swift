419f482e8d7eac973b0179f6aa646bd91c0d113c
JP Wright <jp@contentful.com>
2017-09-28 16:35:12 +0200

    Update Package.swift to use swift tools-version 4.0

--- Package.swift
+++ Package.swift
@@ -1,10 +1,30 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
     name: "Contentful",
-    dependencies: [
-        .Package(url: "https://github.com/Hearst-DD/ObjectMapper", majorVersion: 2, minor: 2),
-        .Package(url: "https://github.com/jensravens/Interstellar", majorVersion: 2, minor: 1)
+    products: [
+        .library(
+            name: "Contentful",
+            targets: ["Contentful"])
     ],
-    exclude: ["Tests/"]
+    dependencies: [
+        .package(url: "https://github.com/jensravens/Interstellar", .upToNextMinor(from: "2.1.0"))
+//        .package(url: "https://github.com/Quick/Nimble", .upToNextMinor(from: "7.0.2")),
+//        .package(url: "https://github.com/venmo/DVR", .upToNextMinor(from: "1.1.0"))
+    ],
+    targets: [
+        .target(
+            name: "Contentful",
+            dependencies: [
+                "Interstellar"
+            ])
+//        .testTarget(
+//            name: "ContentfulTests",
+//            dependencies: [
+//                "Contentful", 
+//                "Nimble", 
+//                "DVR"
+//            ])
+    ]
 )
